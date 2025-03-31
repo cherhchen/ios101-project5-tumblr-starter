869ab5ec214aa05fe29813d331748b431b9f80f0
cherhchen <54284307+cherhchen@users.noreply.github.com>
2025-03-31 10:23:24 -0700

    completed project with refresh pull

--- ios101-project5-tumblr/ViewController.swift
+++ ios101-project5-tumblr/ViewController.swift
@@ -6,17 +6,63 @@
 import UIKit
 import Nuke
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows for the table
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create the cell
+//        let cell = UITableViewCell()
+        
+        // Get a reusable cell
+        // Returns a reusable table-view cell object for the specified reuse identifier and adds it to the table. This helps optimize table view performance as the app only needs to create enough cells to fill the screen and reuse cells that scroll off the screen instead of creating new ones.
+        // The identifier references the identifier you set for the cell previously in the storyboard.
+        // The `dequeueReusableCell` method returns a regular `UITableViewCell`, so we must cast it as our custom cell (i.e., `as! MovieCell`) to access the custom properties you added to the cell.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+        
+        // Get the post-associated table view row
+        let post = posts[indexPath.row]
+        
+        // Configure the cell (i.e. update UI elements like labels, image views, etc.)
+        cell.summaryLabel?.text = post.summary
+        
+        // Get the first photo in the post's photos array
+        if let photo = post.photos.first {
+            let url = photo.originalSize.url
+            
+            // Load the photo in the image view via Nuke library
+            Nuke.loadImage(with: url, into: cell.postImageView)
+        }
+        
+        
+        // Return the cell for use in the respective table view row
+        return cell
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
 
-
+    // A property to store the posts we fetch
+    private var posts: [Post] = []
+    let refreshController = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        refreshController.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+        tableView.refreshControl = refreshController
         
+        tableView.dataSource = self
         fetchPosts()
     }
 
-
+    @objc func refreshData() {
+       // Simulate data fetching (replace with actual API call)
+       DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+           self.tableView.reloadData()  // Reload table data
+           self.refreshController.endRefreshing()  // Stop refreshing animation
+       }
+    }
 
     func fetchPosts() {
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork/posts/photo?api_key=1zT8CiXGXFcQDyMFG7RtcfGLwTdDjFUJnZzKJaWTmgyK4lKGYk")!
@@ -43,11 +89,12 @@ class ViewController: UIViewController {
 
                     let posts = blog.response.posts
 
-
                     print("✅ We got \(posts.count) posts!")
                     for post in posts {
                         print("🍏 Summary: \(post.summary)")
                     }
+                    self?.posts = posts
+                    self?.tableView.reloadData()
                 }
 
             } catch {
